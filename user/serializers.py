a070f8f7f943f55f3fc179c99f586b4b4f580ace
Taras Nester <tarasnester414@gmail.com>
2022-07-16 16:26:33 +0300

    added permissions

--- user/serializers.py
+++ user/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-class UserCreateSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
@@ -12,3 +12,13 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import UserCreateView, CreateTokenView
+from user.views import UserCreateView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
