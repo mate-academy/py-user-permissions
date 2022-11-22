18c4a1af0b10415156c0a8891ea638ae2403e1ba
Artem <Artemvyhovanec@gmail.com>
2022-11-22 16:37:01 +0200

    solution

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,10 @@
-# write your code here
+from django.urls import path
+from user.views import CreateUserView, CreateTokenView, UserDetailView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", UserDetailView.as_view(), name="manage"),
+]
+
+app_name = "user"
