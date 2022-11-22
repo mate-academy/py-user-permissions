fba407d2cb4569c9617e849aaf84e197f48c948c
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-22 15:27:24 +0200

    Created CreateTokenView and urls login

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import (
+    CreateUserView,
+    CreateTokenView
+)
 
 urlpatterns = [
     path(
         "register/",
         CreateUserView.as_view(),
         name="register"
-    )
+    ),
+    path(
+        "login/",
+        CreateTokenView.as_view(),
+        name="token"
+    ),
 ]
 
 app_name = "user"
