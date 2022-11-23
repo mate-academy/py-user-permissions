fbbfec20bd9ae7e05fb2ea62bc7c624fe9058746
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-23 16:35:12 +0200

    created user register/login/me endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,11 @@
-# write your code here
+from django.urls import path
+
+from user.views import CreateUserView, LoginUserView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="me")
+]
+
+app_name = "user"
