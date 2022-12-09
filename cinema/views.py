5e1767d11a3cbb16f089322bac67a2c4d47429a0
Inna Kushnir <innakushnir63@gmail.com>
2022-12-09 10:42:45 +0200

    Flake8 Solution

--- cinema/views.py
+++ cinema/views.py
@@ -3,8 +3,13 @@ from datetime import datetime
 from django.db.models import F, Count
 from rest_framework import viewsets, generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
     DestroyModelMixin
+)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
