faf1ad480301216277b396211d97c8ccea25284d
AnDy <ansmbox@gmail.com>
2023-02-01 23:10:55 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from rest_framework import viewsets
+from rest_framework import viewsets, exceptions
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
+from cinema.permitions import IsAdminOrIfAuthenticatedReadOnly
 
 from cinema.serializers import (
     GenreSerializer,
@@ -24,21 +27,61 @@ from cinema.serializers import (
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def get_permissions(self):
+        if self.action in ("list", "create"):
+            return [
+                permission()
+                for permission in [IsAdminOrIfAuthenticatedReadOnly]
+            ]
+
+        raise exceptions.NotFound
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def get_permissions(self):
+        if self.action in ("list", "create"):
+            return [
+                permission()
+                for permission in [IsAdminOrIfAuthenticatedReadOnly]
+            ]
+
+        raise exceptions.NotFound
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def get_permissions(self):
+        if self.action in ("list", "create"):
+            return [
+                permission()
+                for permission in [IsAdminOrIfAuthenticatedReadOnly]
+            ]
+
+        raise exceptions.NotFound
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def get_permissions(self):
+        if self.action in ("list", "create", "retrieve"):
+            return [
+                permission()
+                for permission in [IsAdminOrIfAuthenticatedReadOnly]
+            ]
+
+        raise exceptions.MethodNotAllowed(self.action)
 
     @staticmethod
     def _params_to_ints(qs):
@@ -87,6 +130,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         )
     )
     serializer_class = MovieSessionSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
@@ -124,6 +169,18 @@ class OrderViewSet(viewsets.ModelViewSet):
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
+    authentication_classes = (TokenAuthentication,)
+
+    def get_permissions(self):
+        if self.action == "list":
+            return [
+                permission()
+                for permission in [IsAdminOrIfAuthenticatedReadOnly]
+            ]
+        elif self.action == "create":
+            return [permission() for permission in [IsAuthenticated]]
+
+        raise exceptions.NotFound
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
