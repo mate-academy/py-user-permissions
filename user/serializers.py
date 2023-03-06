c14557d891f852dc97f477399b4de9fc4dba2989
Paul Starodub <amorallex@gmail.com>
2023-03-06 14:04:48 +0400

    Fixed user/serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "username", "email", "is_staff")
         read_only = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True}, "min_length": 5}
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data: dict) -> User:
         """Create user with encrypted password"""
