4a0f63d8c9d86b099db2ed6e6fc22845562b3a40
Paul Starodub <amorallex@gmail.com>
2023-03-06 12:36:10 +0400

    Updated cinema/serializers.py with typehint

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -83,7 +83,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
-    def validate(self, attrs):
+    def validate(self, attrs: dict) -> dict:
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
             attrs["row"],
@@ -127,7 +127,7 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ("id", "tickets", "created_at")
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Order:
         with transaction.atomic():
             tickets_data = validated_data.pop("tickets")
             order = Order.objects.create(**validated_data)
