9da6bbaf07156491f6e42561b100df0501e1438b
Paul Starodub <amorallex@gmail.com>
2023-03-07 10:23:17 +0400

    Updated urls.py with obtain_auth_token

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
+from rest_framework.authtoken import views
+
 from user.views import (
     CreateUserView
 )
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", views.obtain_auth_token, name="token"),
 ]
 
 app_name = "user"
