e04219f3c07d6da333ec4a9684b095ee4f55eeb6
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-16 19:02:19 +0100

    Added path for CreateTokenView

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cinema.views import CreateUserView
+from cinema.views import CreateUserView, CreateTokenView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register")
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="login"),
 ]
 
 app_name = "user"
