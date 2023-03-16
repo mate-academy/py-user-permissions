4d076630f3e1e813fe124c226fa72660db5f8b56
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-16 20:11:03 +0100

    Added new path for ManageUserView

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from cinema.views import CreateUserView, CreateTokenView
+from cinema.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
