ae775a1144198e3f03d55e95c68d2d6ae82ac13f
Іван Березянський <mamanadarin@gmail.com>
2023-03-28 19:26:45 +0300

    Reduced lentgh code

--- user/views.py
+++ user/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.settings import api_settings
 
 from user.models import User
@@ -10,14 +9,17 @@ from user.serializers import UserSerializer
 
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
 
 
 class LoginTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
 
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
 
