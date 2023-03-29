2f259866f739bdb89c7241690ae425373d575b50
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-03-29 11:45:52 +0300

    Token Authentication

--- user/serializers.py
+++ user/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data) -> User:
-        return get_user_model().objects.create_user(validated_data)
+        return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data) -> User:
         password = validated_data.pop("password", None)
