2a3837cad56ba27b6c7d1d192d95b2580ce3f4a5
Oleksandr Rozsokha <oleksandrozsokha@gmail.com>
2023-03-29 22:21:54 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -163,3 +163,12 @@ class OrderViewSet(
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
