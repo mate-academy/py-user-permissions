92649954f898ba6bcc5d70cf6926d3860eccc8ba
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-04-06 20:15:07 +0300

    fixed error with OrderViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -4,6 +4,7 @@ from django.db.models import F, Count
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
@@ -156,6 +157,8 @@ class OrderViewSet(
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
