68d3ba08afd41fc291ef5e1e8d9c100b149c7911
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-09 17:28:30 +0300

    'Solution'

---
+++ cinema/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
