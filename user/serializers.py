ae6adf41df3fac5b66f1d39bdcf0dcdb3ef27de7
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-10 18:33:56 +0300

    Created serializer for user detail page

--- user/serializers.py
+++ user/serializers.py
@@ -6,10 +6,19 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ["id", "username", "email", "password", "is_staff"]
-        read_only_fields = ["id", "is_staff"]
         extra_kwargs = {
             "password": {"write_only": True, "min_length": 5}
         }
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get("username", instance.username)
+        password = validated_data.get("password")
+        if password:
+            instance.set_password(password)
+        instance.email = validated_data.get("email", instance.email)
+        instance.is_staff = validated_data.get("is_staff", instance.is_staff)
+        instance.save()
+        return instance
