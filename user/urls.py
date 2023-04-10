58407ff21ac64a4f7acf5805ec5050230855bf60
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-10 18:04:41 +0300

    Created "CreateTokenView" for token generation

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import CreateUserView
+from user.views import (
+    CreateUserView,
+    CreateTokenView
+)
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="token")
 ]
 
 app_name = "user"
