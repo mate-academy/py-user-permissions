280aef026b9c9d2b4f55801f74273152f50e9682
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-10 18:35:09 +0300

    Added url for user detail page

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from rest_framework.authtoken import views
 
 from user.views import (
     CreateUserView,
-    CreateTokenView
+    CreateTokenView,
+    ManageUser
 )
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token")
+    path("login/", CreateTokenView.as_view(), name="token"),
+    path("me/", ManageUser.as_view(), name="manage")
 ]
 
 app_name = "user"
