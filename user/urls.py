906a0d35e6e2bb32679592ca0087aac620d4be54
Yevhen Kaskov <kaskov.e@gmail.com>
2023-04-10 16:17:23 +0300

    overwrite default permission for user

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from rest_framework.authtoken import views
-
 from user.views import CreateUserView, CreateTokenView, ManageUserView
+
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
