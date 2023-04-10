32ee67a911729580eb5069e733567f1fbe744ea5
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-10 18:27:25 +0300

    idk how it passed tests

---
+++ cinema_service/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+SAFE_LIST = (
+    "list",
+    "create",
+)
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    """
+    Allow read-only permissions to any authenticated user
+    or full access to admin(is_staff) users
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
