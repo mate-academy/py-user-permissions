7d61a13c9bd2fe2603db515ff8ae1951ad1cf7b9
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-10 18:34:48 +0300

    Created ManageUser for handling user detail page

--- user/views.py
+++ user/views.py
@@ -1,12 +1,23 @@
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUser(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return self.request.user
 
 
 class CreateTokenView(ObtainAuthToken):
