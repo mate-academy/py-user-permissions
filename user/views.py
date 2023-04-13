654e1e509f62b22c646a4fc6e75403bf5704c954
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-13 16:07:15 +0200

    Change user view permission_class

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
@@ -15,10 +16,10 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
+@permission_classes([IsAuthenticated])
 class ProfileUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
