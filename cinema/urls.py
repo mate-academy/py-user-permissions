343a7e4bbcf6a6d79c4325466655f681ca5b82da
Alina Boichenko <ilub1108@gmail.com>
2023-05-12 16:15:50 +0300

    Change path to OrderViewSet

--- cinema/urls.py
+++ cinema/urls.py
@@ -13,12 +13,12 @@ from cinema.views import (
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
-router.register("orders", OrderViewSet)
 
 urlpatterns = [
     path("genres/", GenreViewSet.as_view(), name="genre-list"),
     path("cinema_halls/", CinemaHallViewSet.as_view(), name="cinemahall-list"),
-    path("actors/", ActorViewSet.as_view(), name="actor-list")
+    path("actors/", ActorViewSet.as_view(), name="actor-list"),
+    path("orders/", OrderViewSet.as_view(), name="order-list")
 ] + router.urls
 
 app_name = "cinema"
