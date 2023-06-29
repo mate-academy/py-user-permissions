fd147dadf18fe627f582924331f2ca3d221b4255
Vladyslav Khokhlov <63713268+vlamuha@users.noreply.github.com>
2023-06-29 10:47:01 +0300

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -8,7 +8,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
-from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
+from cinema.permission import IsAdminOrIfAuthenticatedReadOnly
 
 from cinema.serializers import (
     GenreSerializer,
