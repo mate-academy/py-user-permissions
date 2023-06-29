999910168b24cbabe2e05dee6da472ab8617d07a
Vladyslav Khokhlov <63713268+vlamuha@users.noreply.github.com>
2023-06-29 10:44:44 +0300

    Update settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -143,6 +143,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+        "cinema.permission.IsAdminOrIfAuthenticatedReadOnly",
     ],
 }
