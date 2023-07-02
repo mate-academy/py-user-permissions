8307e96a0532ac7750452e8f976667952fc66719
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-02 16:24:41 +0200

    fixed

--- cinema/views.py
+++ cinema/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import NotFound, MethodNotAllowed
 from rest_framework.pagination import PageNumberPagination
@@ -25,49 +25,49 @@ from cinema.serializers import (
 from user.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
-class GenreViewSet(viewsets.ModelViewSet):
+class GenreViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     authentication_classes = (TokenAuthentication, )
-
-    def get_permissions(self):
-        if self.action in ("list", "create"):
-            return [IsAdminOrIfAuthenticatedReadOnly()]
-        raise NotFound()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class ActorViewSet(viewsets.ModelViewSet):
+class ActorViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
     authentication_classes = (TokenAuthentication, )
-
-    def get_permissions(self):
-        if self.action in ("list", "create"):
-            return [IsAdminOrIfAuthenticatedReadOnly()]
-        raise NotFound()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class CinemaHallViewSet(viewsets.ModelViewSet):
+class CinemaHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
     authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
-
-    def get_permissions(self):
-        if self.action in ("list", "create"):
-            return [IsAdminOrIfAuthenticatedReadOnly()]
-        raise NotFound()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
     authentication_classes = (TokenAuthentication,)
-
-    def get_permissions(self):
-        if self.action in ("list", "create", "retrieve"):
-            return [IsAdminOrIfAuthenticatedReadOnly()]
-        raise MethodNotAllowed(method=self.request.method)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     @staticmethod
     def _params_to_ints(qs):
@@ -149,18 +149,18 @@ class OrderPagination(PageNumberPagination):
     max_page_size = 100
 
 
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Order.objects.prefetch_related(
         "tickets__movie_session__movie", "tickets__movie_session__cinema_hall"
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
     authentication_classes = (TokenAuthentication,)
-
-    def get_permissions(self):
-        if self.action in ("list", "create"):
-            return [IsAuthenticated()]
-        raise NotFound()
+    permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
