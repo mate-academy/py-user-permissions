7e0bf18afdb855f055195c037310ade772ac3e65
Anton Shpakovych <shpakovych18@gmail.com>
2023-07-31 17:19:45 +0300

    Fixed

--- user/serializers.py
+++ user/serializers.py
@@ -14,10 +14,10 @@ class UserSerializer(serializers.ModelSerializer):
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        password = validated_data.pop("password")
+        password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
 
-        if password:
+        if password is not None:
             user.set_password(password)
             user.save()
         return user
