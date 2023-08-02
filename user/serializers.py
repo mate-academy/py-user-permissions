32bcce4ab335517c5a4d85c4b709fbd273dad8e6
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-02 14:52:12 +0300

    added min length validation for password

--- user/serializers.py
+++ user/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "username", "email", "is_staff", "password")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
