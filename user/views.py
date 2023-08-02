278e24629733076db540c065d0a266185ba3f393
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-02 14:18:19 +0300

    created UserCreateView

--- user/views.py
+++ user/views.py
@@ -1,1 +1,10 @@
-# write your code here
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+
+    serializer_class = UserSerializer
