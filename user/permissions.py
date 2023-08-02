a59e6bd8aa2b01b735bf25538429c747a24b486f
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-02 15:24:16 +0300

    Fixed views and deleted unnecessary permissions

--- user/permissions.py
+++ user/permissions.py
@@ -1,8 +1,6 @@
-from rest_framework.exceptions import NotFound, MethodNotAllowed
 from rest_framework.permissions import (
     BasePermission,
     SAFE_METHODS,
-    IsAuthenticated
 )
 
 
@@ -18,41 +16,3 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
             )
             or (request.user and request.user.is_staff)
         )
-
-    def has_object_permission(self, request, view, obj):
-        raise NotFound("Not enough permission")
-
-
-class MovieSessionReadWritePermissions(
-    IsAdminOrIfAuthenticatedReadOnly, BasePermission
-):
-
-    def has_object_permission(self, request, view, obj):
-        methods = ("DELETE", "PUT", "PATCH")
-        return (
-            (
-                request.method in methods
-                and request.user
-                and request.user.is_staff
-            ) or request.user.is_authenticated
-        )
-
-
-class MoviePermissions(
-    IsAdminOrIfAuthenticatedReadOnly, BasePermission
-):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in ("PUT", "DELETE"):
-            raise MethodNotAllowed(request.method)
-
-        return request.user and (
-            request.user.is_staff
-            or request.user.is_authenticated
-        )
-
-
-class OrderPermissions(
-    IsAuthenticated, IsAdminOrIfAuthenticatedReadOnly
-):
-    pass
