2bea86b0ba252ca1b40dc8e8f6b82e80d0b16136
Eugene Polunin <altahoma@gmail.com>
2023-08-03 19:31:53 +0300

    Simplified inheritance for MovieSessionViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -104,14 +104,7 @@ class MovieViewSet(
         return MovieSerializer
 
 
-class MovieSessionViewSet(
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet
-):
+class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = (
         MovieSession.objects.all()
         .select_related("movie", "cinema_hall")
