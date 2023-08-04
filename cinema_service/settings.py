c9168e615d50d808e98d5d6b7d3d20d4e2a95e9b
Maksym Bezhevets <begik4@gmail.com>
2023-08-04 13:09:54 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -114,6 +114,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
 AUTH_USER_MODEL = "user.User"
 
 # Internationalization
@@ -121,11 +130,11 @@ AUTH_USER_MODEL = "user.User"
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
