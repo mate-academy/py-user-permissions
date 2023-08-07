d2dd1521c806bebe0e8c2abd68e2e85b22c69460
Bohdan Liutko <luitko007@gmail.com>
2023-08-07 05:43:08 +0000

    Used double quotes in settings

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -120,11 +120,11 @@ AUTH_USER_MODEL = "user.User"
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'myapp.permissions.IsAdminOrIfAuthenticatedReadOnly',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "myapp.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ]
 }
 
