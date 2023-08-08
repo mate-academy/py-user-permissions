28776cba7671c5f36c71b5b65c1a76cd7e4e1d84
Illia Smyrnov <ilushasm@gmail.com>
2023-08-08 23:34:47 +0300

    Refactored cinema/permissions.py

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -4,67 +4,45 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view) -> bool:
-        if (
-            request.method in SAFE_METHODS
-            and not view.action == "retrieve"
-        ):
-            return bool(
-                (request.user.is_authenticated and request.user)
-            )
+
+        if request.method in SAFE_METHODS and view.action != "retrieve":
+            return request.user.is_authenticated and request.user
+
         elif request.method == "POST":
-            return bool(
-                request.user and request.user.is_staff
-            )
+            return request.user and request.user.is_staff
+
         raise NotFound(detail="Not found")
 
 
 class IsAdminOrIfAuthenticatedReadAndRetrieve(BasePermission):
     def has_permission(self, request, view) -> bool:
-        if (
-            request.method in SAFE_METHODS
-        ):
-            return bool(
-                (request.user.is_authenticated and request.user)
-            )
+
+        if request.method in SAFE_METHODS:
+            return request.user.is_authenticated and request.user
+
         elif request.method == "POST":
-            return bool(
-                request.user and request.user.is_staff
-            )
+            return request.user and request.user.is_staff
+
         raise MethodNotAllowed(method=request.method)
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view) -> bool:
-        return bool(
-            (
-                request.user
-                and request.user.is_staff
-            ) or (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-            )
-        )
+
+        if request.method in SAFE_METHODS and request.user.is_authenticated:
+            return True
+        return request.user and request.user.is_staff
 
 
 class IsAdminOrIfAuthenticatedAndPost(BasePermission):
     def has_permission(self, request, view) -> bool:
-        if (
-            request.method in SAFE_METHODS
-            and view.action != "retrieve"
-        ):
-            return bool(
-                (
-                    request.user.is_authenticated
-                    and request.user
-                )
-            )
+
+        if request.method in SAFE_METHODS and view.action != "retrieve":
+            return request.user.is_authenticated
+
         elif request.method == "POST":
-            return bool(
-                (request.user and request.user.is_staff)
-                or (
-                    request.user
-                    and request.user.is_authenticated
-                )
+            return request.user and (
+                request.user.is_staff or request.user.is_authenticated
             )
+
         raise NotFound(detail="Not found")
