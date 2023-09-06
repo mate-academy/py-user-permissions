9dac3e111ee2ffae757f6b6b44e6157e77a22ef7
Taras Savchyn <savik1992@gmail.com>
2023-09-06 23:26:55 +0300

    implement authorization

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-from user.views import CreateUserView, ManageUserView
+from user.views import (
+	CreateUserView, ManageUserView,
+	# ManageUserView
+)
 
 urlpatterns = [
 	path("register/", CreateUserView.as_view(), name="create"),
-	path("login/", views.obtain_auth_token, name="token"),
+	path("login/", views.obtain_auth_token, name="login"),
 	path("me/", ManageUserView.as_view(), name="manage")
 ]
 
