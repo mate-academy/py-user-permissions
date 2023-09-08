82c71181d5af3c3e8fba2fec0183ecc4f78395ef
Taras Savchyn <savik1992@gmail.com>
2023-09-08 17:27:59 +0300

    rewrite get_permission use inline return and use SAFE_METHODS

--- user/permissions.py
+++ user/permissions.py
@@ -3,11 +3,8 @@ from rest_framework import permissions
 
 class IsAdminOrIfAuthenticatedReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user and request.user.is_staff:
-            return True
-
-        if request.user and request.user.is_authenticated:
-            if request.method == "GET":
-                return True
-
-        return False
+        return (
+            True if request.user and request.user.is_staff
+            or request.user and request.user.is_authenticated
+            and request.method in permissions.SAFE_METHODS else False
+        )
