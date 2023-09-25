21d0cac2485204b2f55bb4576a1bd6e1aecd1bba
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-09-25 12:28:04 +0100

    solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path, include
 from rest_framework import routers
 
 from cinema.views import (
@@ -17,6 +18,6 @@ router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "cinema"
