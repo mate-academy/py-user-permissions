69b19f616d3f02bca7d1e4b4a0b9bb29bd5bbafa
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-02 20:06:57 +0300

    Include user/urls.py in project and set URL path for UserCreateView

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,10 @@
-# write your code here
+from django.urls import path
+
+from .views import UserCreateView
+
+
+urlpatterns = [
+    path("register", UserCreateView.as_view(), name="create")
+]
+
+app_name = "user"
