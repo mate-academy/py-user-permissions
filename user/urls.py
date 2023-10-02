73c7860d5ac82497b2022c7ec3502e4f4b3a3d0b
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-02 20:51:06 +0300

    Create ManageUserView and set URl path for it

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import UserCreateView, CreateTokenView
+from .views import (
+    UserCreateView,
+    CreateTokenView,
+    ManageUserView,
+)
 
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token")
+    path("login/", CreateTokenView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
