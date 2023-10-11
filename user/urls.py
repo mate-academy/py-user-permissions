4eb24ffc65932e426ef9f4f644dcade8e7c1de85
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-11 21:39:03 +0300

    add path for register/, login/, me/

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
 	path("register/", CreateUserView.as_view(), name="create"),
-	# path("login/"),
-	# path("me/"),
+	path("login/", CreateTokenView.as_view(), name="login"),
+	path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
