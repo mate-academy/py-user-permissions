79cc6345b053bdbd4fda6d544695caf807a05c63
Max Katkalov <makskatkalov@gmail.com>
2023-10-17 12:57:47 +0300

    user app blacked

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from .views import CreateUserView, CreateTokenView, ManageUserView
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
-    path("me/", ManageUserView.as_view(), name="manage")
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 
