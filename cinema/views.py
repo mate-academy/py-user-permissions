7421a388c61e24628f3ce631a05ab551a75d0ada
Vlad Kleshko <vkoesko@gmail.com>
2023-10-24 20:07:20 +0300

    Restored chenges in cinema.serializers & cinema/models & cinema/views

--- cinema/views.py
+++ cinema/views.py
@@ -21,7 +21,7 @@ from cinema.serializers import (
     OrderSerializer,
     OrderListSerializer,
 )
-from user.permissions import IsAdminOrIfAuthenticatedReadOnly
+from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
 class GenreViewSet(
@@ -110,7 +110,9 @@ class MovieSessionViewSet(
         MovieSession.objects.all()
         .select_related("movie", "cinema_hall")
         .annotate(
-            tickets_available=F("cinema_hall__rows") * F("cinema_hall__seats_in_row") - Count("tickets")
+            tickets_available=F("cinema_hall__rows")
+                              * F("cinema_hall__seats_in_row")
+                              - Count("tickets")
         )
     )
     serializer_class = MovieSessionSerializer
