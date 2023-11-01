af4353571a4451db792eee3d45afd65b038fbda6
Mykola <nickkozlov90@gmail.com>
2023-11-01 19:55:00 +0200

    Crete ManageUserView

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import CreateUserView, CreateTokenView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register"),
-    path("login/", CreateTokenView.as_view()),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
