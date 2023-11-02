1612902f9f49542641c90baa97234f248591486e
Mykola <nickkozlov90@gmail.com>
2023-11-02 20:10:16 +0200

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -121,11 +121,11 @@ AUTH_USER_MODEL = "user.User"
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Kiev"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
