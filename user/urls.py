127ed0f10305110460bda468a808b2f76215d80a
Vlad Larin <lordgadyka71@gmail.com>
2023-11-05 12:24:26 +0200

    Add view & serializer for registration

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,9 @@
-# write your code here
+from django.urls import path
+
+from user.views import UserCreateView
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="create"),
+]
+
+app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,1 +1,7 @@
-# write your code here
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
