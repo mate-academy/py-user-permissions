d85f25ff12a6102a15c55dbc3c09aee8b7439111
Vlad Larin <lordgadyka71@gmail.com>
2023-11-05 12:36:34 +0200

    Add CreateTokenView

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import UserCreateView
+from user.views import UserCreateView, CreateTokenView
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="token"),
 ]
 
 app_name = "user"
