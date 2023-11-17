129a94ea562ad605e821526316a9c042a88e9c61
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-17 16:59:40 +0200

    Create ser serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,1 +1,15 @@
-# write your code here
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fie = ("id", "is_staff")
+        extra_kwargs = {"password": {"write_only": True}, "min_lenght": 5}
+
+        def create(self, validation_data):
+            return get_user_model().object.create_user(**validation_data)
+
