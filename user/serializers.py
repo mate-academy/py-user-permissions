35ab47347b42c6ed5ac11c375a1393fcb8126829
Kostia <kostia3507@gmail.com>
2023-12-04 15:45:16 +0200

    added password's length validation to extra kwargs

--- user/serializers.py
+++ user/serializers.py
@@ -8,16 +8,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "username", "password", "email", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        password = validated_data["password"]
-
-        if len(password) < 5:
-            raise serializers.ValidationError(
-                {"password": ["Length of password must be > than 5!"]}
-            )
-
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
@@ -25,12 +18,6 @@ class UserSerializer(serializers.ModelSerializer):
         user = super().update(instance, validated_data)
 
         if password:
-
-            if len(password) < 5:
-                raise serializers.ValidationError(
-                    {"password": ["Length of password must be > than 5!"]}
-                )
-
             user.set_password(password)
             user.save()
 
