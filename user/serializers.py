91be9b02142f92c6d56031a680107e5d7a8d189a
Serhii Fedorov <fedorov.od.ua@gmail.com>
2023-12-04 23:43:02 +0200

    'Solution'

--- user/serializers.py
+++ user/serializers.py
@@ -1,1 +1,23 @@
-# write your code here
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=5, write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "password", "email", "is_staff")
+        read_only_fields = ("id", "is_staff")
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password is not None:
+            user.set_password(password)
+            user.save()
+        return user
--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,9 @@
-# write your code here
+from django.urls import path
+from .views import UserRegisterView, UserLoginView, UserProfileView
+
+
+urlpatterns = [
+    path("register/", UserRegisterView.as_view(), name="create"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("me/", UserProfileView.as_view(), name="manage"),
+]
