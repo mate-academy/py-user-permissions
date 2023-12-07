0111cb5d314c9045ae7b7ec166a33074626cf35e
Kostia <kostia3507@gmail.com>
2023-12-07 23:40:13 +0200

    fixed cusom permission & added transaction decorator to update methon in userserializer

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -4,7 +4,10 @@ from rest_framework import permissions
 class IsAdminOrIfAuthenticatedReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
-            request.method in
-            permissions.SAFE_METHODS and request.user.is_authenticated
-            or request.user.is_staff
+            (
+                request.method in permissions.SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
         )
