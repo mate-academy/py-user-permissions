38e5eb40f6a7fdf8223d26678dc580c974ef461c
Ruslan Kazmiryk <callogan217@gmail.com>
2023-12-27 19:06:41 +0200

    Change GenreViewSet URL-mapping from router-based to url-patterns

--- cinema/urls.py
+++ cinema/urls.py
@@ -11,13 +11,15 @@ from cinema.views import (
 )
 
 router = routers.DefaultRouter()
-router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("genres/", GenreViewSet.as_view(), name="genre-list"),
+]
 
 app_name = "cinema"
