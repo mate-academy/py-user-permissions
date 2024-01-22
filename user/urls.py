33f724aa063f85d4727671eb6afeb7d3a96d3fed
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-22 23:10:57 +0100

    Change names for 'user' urls

--- user/urls.py
+++ user/urls.py
@@ -4,8 +4,8 @@ from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token"),
-    path("me/", ManageUserView.as_view(), name="profile"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
