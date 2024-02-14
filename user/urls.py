07a7a2660273a20b2604689dbb40b3a43533a537
Maxim V <m4k51m15k4m@gmail.com>
2024-02-14 14:59:36 +0200

    implement api/user/login/

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
 urlpatterns = [
     path("register/",
          CreateUserView.as_view(),
          name="create"),
+    # path("login/",
+    #      views.obtain_auth_token,
+    #      name="token"),
+    path("login/",
+         CreateTokenView.as_view(),
+         name="token"),
 ]
 
 app_name = "user"
