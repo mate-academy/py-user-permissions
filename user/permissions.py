c3215b9850966968488630f0ef9bc9969354e24e
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-02-19 21:11:14 +0200

    Solution 2

--- user/permissions.py
+++ user/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission
 
+
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
