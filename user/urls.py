74f4e39e7dbf656d4f53fcba64fd2be5ba7e4a0e
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-23 12:10:47 +0200

    feat: create login/ endpoint in user API for token

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import (
+    CreateUserView,
+    CreateTokenView
+)
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register")
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="login")
 ]
 
 app_name = "user"
