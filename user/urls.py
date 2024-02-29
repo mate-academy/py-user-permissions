683d14ea1eff8ca24063653162dd04a9fdb724db
LeBwa <lebwa1337@gmail.com>
2024-02-29 20:19:00 +0200

    flake8

--- user/urls.py
+++ user/urls.py
@@ -4,8 +4,8 @@ from user.views import CreateUserView, ObtainTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path('login/', ObtainTokenView.as_view(), name='login'),
-    path("me/", ManageUserView.as_view(), name='manage'),
+    path("login/", ObtainTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
