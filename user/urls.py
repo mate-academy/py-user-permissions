776039a1e62760a77341a03aa16c4f82673fb1fa
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-15 23:37:04 +0200

    fix url names in user app

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from rest_framework.authtoken import views
 from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register"),
-    path("login/", CreateTokenView.as_view(), name="token"),
-    path("me/", ManageUserView.as_view(), name="me"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
