9fc70bffbfa5facaeefa096ad65b8d28bfdf8903
barabarinov <barabarinov@gmail.com>
2024-03-19 15:24:45 +0200

    fixed url names according to tests

--- user/urls.py
+++ user/urls.py
@@ -10,6 +10,6 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="obtain-token"),
-    path("me/", ManageUserView.as_view(), name="manage-user"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
