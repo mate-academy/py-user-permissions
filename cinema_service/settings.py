4a7545fedc85486f22a9d4e6060c915fef3a37d4
Nick Illich <nickillich96@gmail.com>
2024-03-22 05:28:49 +0200

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,9 +139,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
-    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ]
