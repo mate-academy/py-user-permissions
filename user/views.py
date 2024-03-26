3dcfd3d8c3200d93633cf914c43846679772c0d4
Yevhen <evgeniy.playid@icloud.com>
2024-03-26 14:11:20 +0200

    Implement suggested corrections

--- user/views.py
+++ user/views.py
@@ -1,9 +1,13 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
+from user.models import User
 from user.serializers import UserSerializer
 
 
@@ -13,12 +17,29 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username and password:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return Response({'error': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+            if user.check_password(password):
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key})
+
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
