15ed25fcfaeabc97af43913828d9e94263933ba6
Yevhen <evgeniy.playid@icloud.com>
2024-03-26 14:12:46 +0200

    Correct flake8 mistakes

--- user/views.py
+++ user/views.py
@@ -20,20 +20,24 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         if username and password:
             try:
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
-                return Response({'error': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    {"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND
+                )
 
             if user.check_password(password):
                 token, created = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key})
+                return Response({"token": token.key})
 
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
