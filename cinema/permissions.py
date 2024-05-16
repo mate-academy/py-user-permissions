9de7369c263475a587b85382b58d2c83e4a98f8e
vital <vitaliyburkalo81@gmail.com>
2024-05-16 14:09:29 +0300

    updated permissions py & urls py & views py

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -3,8 +3,11 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(
+        return (
             request.method in SAFE_METHODS
+            or (request.user and request.user.is_authenticated)
+        ) or (
+            request.method == "POST"
             and request.user and request.user.is_authenticated
         ) or (
             request.user and request.user.is_staff
