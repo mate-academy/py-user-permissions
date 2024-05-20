1637e85c0ba8d86cd5869beb5d5ea80fbeaa4282
Oleksandra <aleksandra@regdesk.co>
2024-05-20 17:17:58 +0300

    fixed

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from user.views import CreateUserViewSet, LoginUserView, ManageUserView
 
 urlpatterns = [
-    path("register", CreateUserViewSet.as_view(), name="create"),
+    path("register/", CreateUserViewSet.as_view(), name="create"),
     path("login/", LoginUserView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage")
 ]
