d5ecf37899ba7e56008581b991620da4e8f67909
kekW <panicua12@gmail.com>
2024-05-29 14:32:52 +0300

    chore: couldn't see test result because of addon. Fixed it

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("api/user/", include("user.urls", namespace="user")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
