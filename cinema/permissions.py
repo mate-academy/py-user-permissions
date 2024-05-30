20a8f4f61710cfb8879caf04552024b6338b3d4c
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-30 15:02:04 +0300

    Refactor permissions.py
    Fix flake8 error:
     - W504 line break after binary operator

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -5,7 +5,7 @@ class IsAdminAllOrIsAuthenticatedOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS and
-            request.user and
-            request.user.is_authenticated
+            request.method in SAFE_METHODS
+            and request.user
+            and request.user.is_authenticated
         ) or (request.user and request.user.is_staff)
