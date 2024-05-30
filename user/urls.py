b79ce85db68336e4efd6ced610fec7ff0fd41465
MykolaMazhula <mykm3ua@gmail.com>
2024-05-30 19:32:59 +0200

    Create POST api/user/register/

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,7 @@
-# write your code here
+from django.urls import path
+
+from user.views import CreateUserView
+
+app_name = "user"
+
+urlpatterns = [path("register/", CreateUserView.as_view(), name="create")]
