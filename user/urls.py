0dbb7ae3d55ebbd283f61c4a2235a0d992237706
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-31 15:19:14 +0300

    fixed Flake8

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from user.views import CreateUserView, CreateUserView, ManageUserView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("register/", CreateTokenView.as_view(), name="create"),
     path("login/", CreateUserView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
