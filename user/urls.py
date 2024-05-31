c8b0bb8a6c0f4fa8bc1e664f87a65110297070a5
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-31 15:21:36 +0300

    Fixed tests

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateTokenView.as_view(), name="create"),
-    path("login/", CreateUserView.as_view(), name="login"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
