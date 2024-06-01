62a15f404600fd1b3930183dd10e7ab6dcb36c63
MykolaMazhula <mykm3ua@gmail.com>
2024-06-01 16:37:19 +0200

    Create user info with CurrentUserListUpdateView

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CurrentUserListUpdateView
 
 app_name = "user"
 
-urlpatterns = [path("register/", CreateUserView.as_view(), name="create")]
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", views.obtain_auth_token, name="token"),
+    path("me/", CurrentUserListUpdateView.as_view(), name="me"),
+]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,19 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CurrentUserListUpdateView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [
+        TokenAuthentication,
+    ]
+
+    def get_object(self):
+        return self.request.user
