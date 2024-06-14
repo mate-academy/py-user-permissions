744d007a5a931ef8f3e97f45e104d8af1ee44e99
bwaah1 <chykivan@gmail.com>
2024-06-14 15:55:13 +0300

    Fix FieldError in cinema views

--- cinema/views.py
+++ cinema/views.py
@@ -95,7 +95,7 @@ class MovieSessionViewSet(mixins.ListModelMixin,
         MovieSession.objects.all()
         .select_related("movie", "cinema_hall")
         .annotate(
-            tickets_available=F("cinema_hall__row")
+            tickets_available=F("cinema_hall__rows")
             * F("cinema_hall__seats_in_row")
             - Count("tickets")
         )
