4aeff6c04fc297c9f41a89cebbf371f6c10db52e
Anton Konyk <antonkonyk.com>
2024-06-18 19:07:53 +0200

    add path login with LoginUserView

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
-
+from user.views import CreateUserView, LoginUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="get_token"),
 ]
 
 app_name = "user"
