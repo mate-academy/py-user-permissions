6939735b57202ae07c4f6d03e202ef97bdcd99c4
Anton Konyk <antonkonyk.com>
2024-06-18 20:21:23 +0200

    create ManageUserView

--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserListView
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -11,3 +13,20 @@ class CreateUserView(generics.CreateAPIView):
 
 class LoginUserView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def get_serializer(self, *args, **kwargs):
+        if self.request.method == 'GET':
+            serializer_class = UserListView
+            kwargs.setdefault('context', self.get_serializer_context())
+            return serializer_class(*args, **kwargs)
+
+        return super().get_serializer(*args, **kwargs)
