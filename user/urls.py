1707b1aadcf8a3114c31a33d6d538ba657babcab
Anton Konyk <antonkonyk.com>
2024-06-18 20:23:17 +0200

    add puth me/, change name for login/ path

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from user.views import CreateUserView, LoginUserView
+from user.views import (
+    CreateUserView,
+    LoginUserView,
+    ManageUserView
+)
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="get_token"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("my/", ManageUserView.as_view(), name="manage")
 ]
 
 app_name = "user"
