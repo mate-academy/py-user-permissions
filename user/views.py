371967a67d0f543bbabb63cd96463d0896a38710
dmitriy kim <d.villarionovich@gmail.com>
2024-06-22 20:21:46 +0200

    Flake8 fixed

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
@@ -9,9 +9,11 @@ from user.serializers import UserSerializer
 
 class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class UserLoginView(ObtainAuthToken):
+    permission_classes = (AllowAny,)
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
