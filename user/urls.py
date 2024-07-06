34676b66f97007df343a6d5b36cdd73e216af976
Alisa <alisasklrv@gmail.com>
2024-07-06 18:11:57 +0300

    solution

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from user.views import CreateUserView, LoginUserView, ManageUserView
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", LoginUserView.as_view(), name="login"),
-    path("me/", ManageUserView.as_view(), name="manage_user")
+    path("me/", ManageUserView.as_view(), name="manage")
 ]
 
 app_name = "user"
