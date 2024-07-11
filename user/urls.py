41f30178064a38be3005c165dadbde6e3affa870
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-11 19:22:50 +0300

    Create serializers, vies and endpoints for User model

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,12 @@
-# write your code here
+from django.urls import path
+
+from user.views import CreateUserView, LoginUserView, MangerUserView
+
+
+urlpatterns =[
+    path("register/", CreateUserView.as_view(), name="register_user"),
+    path("login/", LoginUserView.as_view(), name="login_user"),
+    path("me/", MangerUserView.as_view(), name="manage_user"),
+]
+
+app_name = "user"
