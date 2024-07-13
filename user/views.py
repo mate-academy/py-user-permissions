b64c2fa583e1bbe292fdcb262e48c8ea0a612326
Roma Nesterenko <rom.puh@gmail.com>
2024-07-13 12:43:34 +0300

    Redefined permission_classes in CreateUserView

--- user/views.py
+++ user/views.py
@@ -8,6 +8,7 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = []
 
 
 class CreateTokenView(ObtainAuthToken):
