945988c74f9f30b1bc619481aa0f83023bd64959
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-07-31 18:15:34 +0300

    change views for user and urls

--- user/urls.py
+++ user/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 
-from user.views import (
-    CreateUserView,
-    LoginUserView,
-    ManageUserView,
-)
+from user.views import CreateUserView, CreateTokenView, ManageUserViewSet
+
+app_name = "user"
 
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register"),
-    path("login/", LoginUserView.as_view(), name="login"),
-    path("me/", ManageUserView.as_view(), name="profile"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserViewSet.as_view(), name="manage")
 ]
-
-app_name = "user"
