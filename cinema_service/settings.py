163045cbf7ad7e563515815f9caca7715a4371c4
TarasFirst <firsttaras@mail.com>
2024-08-19 23:08:05 +0200

    set REST_FRAMEWORK PERMISSION and AUTHENTICATION

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,3 +137,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": (
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+}
