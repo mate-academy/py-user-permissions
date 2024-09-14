fa19008b65ae5e134e4407183dfa264044205ace
mykyta <nikita.fedotov222@gmail.com>
2024-09-14 15:44:10 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
@@ -31,7 +30,7 @@ class GenreViewSet(
 ):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly,]
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly, ]
 
 
 class ActorViewSet(
@@ -41,7 +40,7 @@ class ActorViewSet(
 ):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly,]
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly, ]
 
 
 class CinemaHallViewSet(
@@ -62,7 +61,7 @@ class MovieViewSet(
 ):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly,]
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly, ]
 
     @staticmethod
     def _params_to_ints(qs):
@@ -118,7 +117,7 @@ class MovieSessionViewSet(
         )
     )
     serializer_class = MovieSessionSerializer
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly,]
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly, ]
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
@@ -160,7 +159,7 @@ class OrderViewSet(
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly,]
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly, ]
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
