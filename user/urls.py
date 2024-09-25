868c8f4f3402dec6be9515dd0e666704310112e1
Максим <www.depaton@gmail.com>
2024-09-25 04:47:05 +0300

    User register, login, update.

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,10 @@
-# write your code here
+from django.urls import path
+from user.views import CreateUserView, CreateTokenView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view()),
+    path("login/", CreateTokenView.as_view()),
+    path("me/", ManageUserView.as_view()),
+]
+
+app_name = 'user'
