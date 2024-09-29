cb287be0eaa1e0f196253003cb45c59fdc254ab3
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-29 19:23:28 +0300

    minor fix

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import authenticate
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in SAFE_METHODS
-            and request.user
-            and request.user.is_authenticated
-        ) or (request.user and request.user.is_staff)
+        user = request.user
+        auth_constraints = all(
+            (request.method in SAFE_METHODS, user, user.is_authenticated)
+        )
+        staff_constraints = all((user, user.is_staff))
+        return auth_constraints or staff_constraints
