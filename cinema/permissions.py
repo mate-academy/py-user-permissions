ff68ec1ffb7b61b3a026849621edb0d15dbb676c
Vlada Bilai <vlada.belayy@gmail.com>
2024-10-02 20:13:00 +0300

    first commit

---
+++ cinema/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminAllORAuthenticatedORReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        auth_constraints = all(
+            (request.method in SAFE_METHODS, user, user.is_authenticated)
+        )
+        staff_constraints = all((user, user.is_staff))
+        return auth_constraints or staff_constraints
