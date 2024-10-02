c06f78415ed8a27892a23697f2f373e10796fbeb
DSahalatyi <dsahalatyi@gmail.com>
2024-10-02 17:54:44 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,5 @@
+# flake8: noqa: E501
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
@@ -63,9 +65,7 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.created_at)
@@ -78,9 +78,7 @@ class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets"
     )
-    order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, related_name="tickets"
-    )
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
     row = models.IntegerField()
     seat = models.IntegerField()
 
@@ -122,8 +120,7 @@ class Ticket(models.Model):
         )
 
     def __str__(self):
-        return (f"{str(self.movie_session)} "
-                f"(row: {self.row}, seat: {self.seat})")
+        return f"{str(self.movie_session)} " f"(row: {self.row}, seat: {self.seat})"
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
