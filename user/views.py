59aca6ee0f1204899b2164abca424a16ef573225
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-03 17:54:35 +0300

    User fix

--- user/views.py
+++ user/views.py
@@ -1,7 +1,5 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
@@ -9,6 +7,8 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = []
+    permission_classes = []
 
 
 class LoginUserView(ObtainAuthToken):
@@ -17,8 +17,6 @@ class LoginUserView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = [TokenAuthentication, ]
-    permission_classes = [IsAuthenticated, ]
 
     def get_object(self):
         return self.request.user
