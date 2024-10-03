e60037f4ab7c0443125dd5b9a52496f6c9d97321
DSahalatyi <dsahalatyi@gmail.com>
2024-10-03 18:05:31 +0300

    Remove unnecessary code

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -5,10 +5,9 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            (request.user and request.user.is_staff)
+            request.user.is_staff
             or (
-                request.user
-                and request.user.is_authenticated
+                request.user.is_authenticated
                 and request.method in SAFE_METHODS
             )
         )
