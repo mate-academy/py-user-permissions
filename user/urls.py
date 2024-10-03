937560e27689cbd9fd5776f2505f3b9bc6e9f86f
Alexandr Politov <apolitov83@gmail.com>
2024-10-03 19:49:13 +0300

    endpoint (+view) 'login/' for user getting token created

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView
 
 
 app_name = "user"
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="user-create")
+    path("register/", CreateUserView.as_view(), name="user-create"),
+    path("login/", LoginUserView.as_view(), name="obtain-token"),
 ]
