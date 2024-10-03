75433770c92b62e768f786b9be324b1ab0decee2
achalaidiuk <sasha.treyser90@gmail.com>
2024-10-03 18:14:00 +0300

    fixed reviewed changes

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -119,7 +119,10 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "user.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
 }
 
 # Internationalization
