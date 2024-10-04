4ff1f8c92422ae419e4d8a6c686b229560138bfb
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-10-04 09:54:21 +0200

    implement py-token-authentication 4

--- user/views.py
+++ user/views.py
@@ -1,14 +1,16 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.settings import api_settings
 
+from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class LoginUserView(ObtainAuthToken):
@@ -17,8 +19,8 @@ class LoginUserView(ObtainAuthToken):
 
 class UserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
