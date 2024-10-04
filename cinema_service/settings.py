06b94a4d03883a5661da451f78c95dc89cc73251
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-10-04 09:24:38 +0200

    implement py-token-authentication 3

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,11 +139,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'cinema.permissions.IsAdminOrIfAuthenticatedReadOnly',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ] ,
 }
-
