8602aaac56a32af61f0d949b68d2624f0d2f6afc
Ihor Utkin <igorutkin2002@gmail.com>
2024-10-04 18:50:44 +0200

    Resolve

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -117,11 +117,11 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'cinema.permissions.IsAdminOrIfAuthenticatedReadOnly',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ]
 }
 
