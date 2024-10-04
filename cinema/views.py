6560427454187ecd00b0d830fb0bb7a6aa7e55a6
Vlada Bilai <vlada.belayy@gmail.com>
2024-10-04 11:04:57 +0300

    Used REST_FRAMEWORK constant

--- cinema/views.py
+++ cinema/views.py
@@ -31,8 +31,6 @@ class GenreViewSet(
 ):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminAllORAuthenticatedORReadOnly,)
 
 
 class ActorViewSet(
@@ -42,8 +40,7 @@ class ActorViewSet(
 ):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminAllORAuthenticatedORReadOnly,)
+
 
 
 class CinemaHallViewSet(
@@ -53,8 +50,7 @@ class CinemaHallViewSet(
 ):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminAllORAuthenticatedORReadOnly,)
+
 
 
 class MovieViewSet(
@@ -65,8 +61,6 @@ class MovieViewSet(
 ):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminAllORAuthenticatedORReadOnly,)
 
     @staticmethod
     def _params_to_ints(qs):
@@ -158,7 +152,6 @@ class OrderViewSet(
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
