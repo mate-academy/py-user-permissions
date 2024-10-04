3424951bbdf3082aed153cc4b38c2cd014c3c883
serhii <ockar2018@gmail.com>
2024-10-04 19:07:03 +0300

    fix

--- user/views.py
+++ user/views.py
@@ -9,15 +9,18 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = ()
+    permission_classes = ()
 
 
 class LoginUserView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    authentication_classes = ()
+    permission_classes = ()
 
 
 class ManagerUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
