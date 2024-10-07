9f795b599ed512be38f207bd1ccdc8dddf233166
struhanets <posseydon87@gmail.com>
2024-10-07 12:57:47 +0200

    Solution

---
+++ cinema/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS and request.user.is_authenticated:
+            return True
+        if request.user.is_staff:
+            return True
+
+        return False
