e7cfe2cc3d18928c6f4d107210579f7250a68664
Alexandr Politov <apolitov83@gmail.com>
2024-10-07 13:35:19 +0300

    unneeded definitions of authentication_classes and permission_classes deleted

--- cinema/views.py
+++ cinema/views.py
@@ -107,8 +107,6 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         )
     )
     serializer_class = MovieSessionSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
