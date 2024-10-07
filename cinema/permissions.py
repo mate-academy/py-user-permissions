02e0bc0d5a94f42815468d2cc257bde67d91b8d9
Andrii <andrii.khamaza0@gmail.com>
2024-10-07 14:43:25 +0300

    Fixed mistakes

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -7,58 +7,8 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     if as a user - read only request"""
 
     def has_permission(self, request, view):
-        view_name = view.__class__.__name__
-        view_action = view.action
-
-        if view_name in [
-            "ActorViewSet",
-            "GenreViewSet",
-            "CinemaHallViewSet",
-        ] and view_action in ["retrieve", "destroy", "update"]:
-            raise NotFound("Object not found.")
-
-        if view_name == "MovieSessionViewSet":
-            return bool(
-                view_action in ("list", "retrieve")
-                and request.user
-                and request.user.is_authenticated
-            ) or (request.user and request.user.is_staff)
-
-        return bool(
-            view_action == "list"
-            and request.user
-            and request.user.is_authenticated
-        ) or (request.user and request.user.is_staff)
-
-
-class ReadCreateOnly(BasePermission):
-
-    def has_permission(self, request, view):
-        if view.action in ["destroy", "update"]:
-            raise MethodNotAllowed(
-                method=request.method,
-                detail="Method not allowed for this action."
-            )
 
         return bool(
             request.method in SAFE_METHODS
-            and request.user
             and request.user.is_authenticated
-        ) or (
-            request.method in ("GET", "POST")
-            and request.user
-            and request.user.is_staff
-        )
-
-
-class ReadAndCreateIfAuthenticated(BasePermission):
-
-    def has_permission(self, request, view):
-        if view.action in ["delete", "update", "retrieve", "destroy"]:
-            raise NotFound("Object not found.")
-
-        return bool(
-            request.method in ("GET", "POST")
-            and request.user
-            and request.user.is_authenticated
-        )
+        ) or (request.user and request.user.is_staff)
