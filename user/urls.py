c3c0fd2c181047e38a40cb8afed5471a7d94774b
TsvetanKichuk <toys4babyodua@gmail.com>
2024-10-07 10:08:35 +0200

    add serializer, permissions, urls, views

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,12 @@
-# write your code here
+from django.urls import path
+
+from user.views import CreateUserView, CreateTokenView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
+]
+
+app_name = "user"
+
