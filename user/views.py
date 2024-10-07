96b1b9ed1081f85a3c478ba95c5b36e2e6f5a592
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-10-07 19:35:12 +0200

    implement py-token-authentication 6

--- user/views.py
+++ user/views.py
@@ -1,10 +1,8 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.settings import api_settings
 
-from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 from user.serializers import UserSerializer
 
 
