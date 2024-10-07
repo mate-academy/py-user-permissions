000324ac6529b34fc6d546643aff198efab29a89
Misha Havryliuk <misharob31415@gmail.com>
2024-10-07 13:47:22 +0300

    Solution2.2

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
@@ -9,6 +9,7 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = ()
 
 
 class LoginUserView(ObtainAuthToken):
@@ -17,8 +18,7 @@ class LoginUserView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get_object(self):
+    def get_object(self) -> get_user_model():
         return self.request.user
