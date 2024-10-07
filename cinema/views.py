646b5a9bd5072b2c661ea0026bebf01b4895cc1c
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-10-07 14:39:36 +0300

    "fixed imports"

--- cinema/views.py
+++ cinema/views.py
@@ -2,13 +2,10 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
-from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
-
 from cinema.serializers import (
     GenreSerializer,
     ActorSerializer,
