d1f9b950e276a1589dd0c7046a3e1b5cd19dd13a
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-09 09:16:03 +0300

    'Solution4'

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
