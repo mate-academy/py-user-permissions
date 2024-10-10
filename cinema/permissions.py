05d4b613f126ca268142ec4322e5de03909dc507
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-10 12:18:15 +0300

    'Solution5'

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -10,7 +10,7 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     """
 
     def has_permission(self, request: Request, view: View) -> bool:
-        return bool(
+        return (
             (request.method in SAFE_METHODS and request.user.is_authenticated)
             or request.user.is_staff
         )
