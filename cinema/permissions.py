dcab076b8b070e864a2386c509716855818cf954
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-10-12 22:05:47 +0300

    "flake8"

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -5,6 +5,6 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in SAFE_METHODS
-                and request.user.is_authenticated
+            request.method in SAFE_METHODS
+            and request.user.is_authenticated
         ) or request.user.is_staff
