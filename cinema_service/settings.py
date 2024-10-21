cb9053c004e4e3e51f69da36285f33220fb7a5e4
Bogdan <bogdan.rylov422@gmail.com>
2024-10-21 11:08:45 +0300

    Reformat code according to mentor's requirements

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,5 +139,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication"
     ]
 }
