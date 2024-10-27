b12b0703f772ec2a17420b4e8ea3da7b10d8bddb
Olga-Gynguliak <olga.gynguliak@gmail.com>
2024-10-27 19:12:47 +0200

    Solution "py-token-authentication_version3"

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAdminOrAuthenticatedReadOnly(BasePermission):
+
+class IsAdminOrIsAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
             (
