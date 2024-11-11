2e04c2f37ab25b9e4f3bc742dff360496f237273
Viesich <v.s.vesich@gmail.com>
2024-11-11 19:23:01 +0200

    remove None from validated_datas tuple in user/serializers

--- user/serializers.py
+++ user/serializers.py
@@ -19,7 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        password = validated_data.pop("password", None)
+        password = validated_data.pop("password", )
         user = super().update(instance, validated_data)
         if password:
             user.set_password(password)
