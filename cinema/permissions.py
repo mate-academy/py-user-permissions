2dd49cb83a3ba929034966136f15860d6ab79d4d
Arsen <arsen.test.mess@gmail.com>
2024-11-12 17:36:47 +0200

    Solution

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -6,14 +6,11 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
         if request.user and request.user.is_staff:
             return True
 
-            # Разрешить аутентифицированным пользователям доступ к безопасным методам (чтение)
         if request.method in SAFE_METHODS:
             return request.user and request.user.is_authenticated
 
-            # Разрешить аутентифицированным пользователям создание заказов (POST) только в OrderViewSet
         if (view.__class__.__name__ == "OrderViewSet"
                 and request.method == "POST"):
             return request.user and request.user.is_authenticated
 
-            # Запретить все остальные действия для неадминистраторов
         return False
