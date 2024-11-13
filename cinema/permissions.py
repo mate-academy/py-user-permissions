07e84ab04f71ba5c8bdc3f65d5e52c8917d36b0f
Viesich <v.s.vesich@gmail.com>
2024-11-13 10:16:27 +0200

    create IsAdminOrIfAuthenticatedReadOnly permissions for views & add it to views

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -3,10 +3,6 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in ["GET", "HEAD", "OPTIONS"]:
-            return bool(
-                request.method in
-                SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-            ) or (request.user and request.user.is_staff)
+        if request.method in SAFE_METHODS and request.user.is_authenticated:
+            return True
+        return bool(request.user and request.user.is_staff)
