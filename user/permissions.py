12fcaf65c5861b0f516d0f4aa50a3be340088254
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-14 08:18:34 +0200

    flake8

--- user/permissions.py
+++ user/permissions.py
@@ -5,11 +5,11 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
             (
-                    request.method in SAFE_METHODS
-                    and request.user
-                    and request.user.is_authenticated
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
             )
             or (
-                    request.user and request.user.is_staff
+                request.user and request.user.is_staff
             )
         )
