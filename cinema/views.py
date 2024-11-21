cd153365a4ebc151b23482bd48242f73d7400479
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-11-21 22:56:39 +0200

    flake8

--- cinema/views.py
+++ cinema/views.py
@@ -110,9 +110,11 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         MovieSession.objects.all()
         .select_related("movie", "cinema_hall")
         .annotate(
-            tickets_available=F("cinema_hall__rows")
-            * F("cinema_hall__seats_in_row")
-            - Count("tickets")
+            tickets_available=(
+                F("cinema_hall__rows")
+                * F("cinema_hall__seats_in_row")
+                - Count("tickets")
+            )
         )
     )
     serializer_class = MovieSessionSerializer
@@ -141,7 +143,6 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
-
 
 
 class OrderPagination(PageNumberPagination):
