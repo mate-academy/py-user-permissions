721a9a1a7c3c1d0e692e38c052f553755b2fa5e7
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-28 20:08:26 +0200

    Fix password update in user serializer
    Correct the call to set_password by passing the password argument.

--- user/serializers.py
+++ user/serializers.py
@@ -17,7 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = super().update(instance, validated_data)
 
         if password:
-            user.set_password()
+            user.set_password(password)
             user.save()
 
         return user
