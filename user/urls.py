8800873f212c76f999a3e7663576d31e633c19e7
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-28 19:00:44 +0200

    Add user registration API endpoint
    - Implemented a user registration endpoint by creating a UserSerializer, CreateUserView, and URL pattern for the user app.

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,8 @@
-# write your code here
+from django.urls import path
+from user import views
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", views.CreateUserView.as_view(), name="user-register")
+]
