52c566ebd2c192bb5d600fe23e8463ae0a2a13a2
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-28 19:19:18 +0200

    Add user management view with authentication and endpoint
    - Added "ManageUserView" for retrieving and updating user profiles.
    - Added a new endpoint "me/" in the user URL patterns to enable user profile management.

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,6 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", views.CreateUserView.as_view(), name="user-register"),
-    path("login/", views.LoginUserView.as_view(), name="user-login")
+    path("login/", views.LoginUserView.as_view(), name="user-login"),
+    path("me/", views.ManageUserView.as_view(), name="user-update")
 ]
