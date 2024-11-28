aa1aca5a2708948a03d9dfecefd52dcfd08742f7
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-28 20:35:47 +0200

    Update URL names for user app and modify permissions for user views
    - Renamed URL patterns to be make tests pass.
    - Updated permissions for CreateUserView and LoginUserView to allow access to all users.

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from user import views
 app_name = "user"
 
 urlpatterns = [
-    path("register/", views.CreateUserView.as_view(), name="user-register"),
-    path("login/", views.LoginUserView.as_view(), name="user-login"),
-    path("me/", views.ManageUserView.as_view(), name="user-update")
+    path("register/", views.CreateUserView.as_view(), name="create"),
+    path("login/", views.LoginUserView.as_view(), name="login"),
+    path("me/", views.ManageUserView.as_view(), name="manage")
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
@@ -9,10 +9,12 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 
 class LoginUserView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    permission_classes = [AllowAny]
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
