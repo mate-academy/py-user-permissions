9b25ff0921ffd1c644dea1778185d17286e95d89
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-12-07 16:12:34 +0200

    Final Solution

--- cinema/views.py
+++ cinema/views.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
@@ -148,8 +147,6 @@ class OrderViewSet(
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
     def get_permissions(self):
         if self.action == "create":
