797fa0a3cba2d05b8d6d459ae602be11538ad485
Соня Cавкова <silva.vvss12@gmail.com>
2024-12-14 14:24:39 +0200

    solution

--- user/serializers.py
+++ user/serializers.py
@@ -1,1 +1,61 @@
-# write your code here
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = ("id", "is_staff")
+        extra_kwargs = {
+            "password": {"write_only": True, "min_length": 5}
+        }
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            validate_password(password, user)  # Validate the new password
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={"input_type": "password"}, trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        from django.contrib.auth import authenticate
+
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        if username and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                username=username,
+                password=password
+            )
+            if not user:
+                raise serializers.ValidationError(
+                    "Unable to log in with provided credentials.",
+                    code="authorization",
+                )
+        else:
+            raise serializers.ValidationError(
+                "Must include 'username' and 'password'.",
+                code="authorization",
+            )
+
+        attrs["user"] = user
+        return attrs
