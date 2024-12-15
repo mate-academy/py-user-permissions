4ad72ee1493e287717dde39217af7a8aecd1fab2
Matvii Safonov <matveysafonov2@gmail.com>
2024-12-15 23:22:26 +0200

    fix flake8

--- cinema/views.py
+++ cinema/views.py
@@ -109,9 +109,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         MovieSession.objects.all()
         .select_related("movie", "cinema_hall")
         .annotate(
-            tickets_available=F("cinema_hall__rows")
-                              * F("cinema_hall__seats_in_row")
-                              - Count("tickets")
+            tickets_available=F("cinema_hall__rows") # noqa
+                              * F("cinema_hall__seats_in_row") # noqa
+                              - Count("tickets") # noqa
         )
     )
     serializer_class = MovieSessionSerializer
