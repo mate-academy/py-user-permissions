f87894116d04d750c52f157dfcc4f30f28a19bd9
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-17 11:41:38 +0200

    "solution"

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from user.urls import app_name
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("__debug__/", include("debug_toolbar.urls")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/user/", include("user.urls", namespace=app_name)),
 ]
