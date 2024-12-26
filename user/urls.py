76eed03bef3f7e92524d56ed1af10a56f36ee50a
Frezworx <frezworx@gmail.com>
2024-12-26 17:53:29 +0200

    Rename user creation endpoint from 'create' to 'register'.
    Updated the URL pattern to improve clarity and align with common conventions. This change ensures better readability and consistency across the API.

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from user.views import UserCreateView, UserLoginView, ManageUserView
 app_name = "user"
 
 urlpatterns = [
-    path("create/", UserCreateView.as_view(), name="create"),
+    path("register/", UserCreateView.as_view(), name="create"),
     path("login/", UserLoginView.as_view(), name="login"),
 
     path("me/", ManageUserView.as_view(), name="manage"),
