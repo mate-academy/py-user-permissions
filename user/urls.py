37ec580992f4e7e03684aa2b967ebd756f233e90
Frezworx <frezworx@gmail.com>
2024-12-26 16:20:12 +0200

    Add user management views, serializers, and URLs
    Implemented user registration, login, and profile management endpoints using Django Rest Framework. Updated project URLs to include the new user app routes. Added necessary serializers and views to support user creation, authentication, and profile updates.

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,12 @@
-# write your code here
+from django.urls import path
+
+from user.views import UserCreateView, UserLoginView, ManageUserView
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="login"),
+    path("login/", UserLoginView.as_view(), name="obtain-token"),
+
+    path("me/", ManageUserView.as_view(), name="me"),
+]
