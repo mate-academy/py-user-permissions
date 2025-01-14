e344910b8bb0b892cf12b3c2195f766119ff9f18
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-01-14 20:41:07 +0100

    Solution_v4

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,6 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="get_token"),
+    path("login/", LoginUserView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
