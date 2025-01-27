507db48ca637609f45336c1c29b7b8e0de042c4b
zakhar <zaharsavchen@gmail.com>
2025-01-27 12:28:56 +0200

    add default authentication and permission classes

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
@@ -10,15 +9,16 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = ()
 
 
 class LoginUserView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    permission_classes = ()
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self) -> get_user_model():
